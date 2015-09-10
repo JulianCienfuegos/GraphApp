8e2abb26b62398f146d8510cadd004d13cc4d1bd
Julian Aureliano Cienfuegos <melvyniandrag@gmail.com>
2015-09-10 17:28:57 -0400

    clear plot with bugs

--- graph_app.py
+++ graph_app.py
@@ -21,7 +21,8 @@ import Tkinter as Tk
 mpl.rcParams['toolbar'] = 'None'
 
 class GraphApp:
-	def __init__(self, canvas, ax1):	
+	def __init__(self, canvas, fig, ax1):	
+		self.fig = fig
 		self.ax = ax1
 		self.vert_x = []
 		self.vert_y = []
@@ -39,7 +40,7 @@ class GraphApp:
 		self.clicks = 0
 		self.tol = 1e-1
 		self.near = 0
-		self.G = nx.Graph()
+		#self.G = nx.Graph()
 		self.i = 0
 		self.colors = {0:0, 1:50}
 
@@ -52,7 +53,7 @@ class GraphApp:
 			self.vert_x.append(event.xdata)
 			self.vert_y.append(event.ydata) # Dot ready for drawing.
 			self.r.append(100)
-			self.G.add_node(self.i) # Add a node to the graph
+			#self.G.add_node(self.i) # Add a node to the graph
 			self.i += 1
                         self.c.append(0)
 		else: # then the click is close
@@ -68,22 +69,22 @@ class GraphApp:
 				self.edge_Y.append(self.temp_Y)
 				e = (self.vert_x.index(self.temp_X[0]), self.vert_x.index(self.temp_X[1]))
 				edge_idx_tup = (self.vert_x.index(self.temp_X[1]), self.vert_x.index(self.temp_X[0]))
-                                self.edge_colors.append('0.75')
-                                self.G.add_edge(*e)
+				self.edge_colors.append('0.75')
+				#self.G.add_edge(*e)
 				self.temp_X = []
 				self.temp_Y = []
 			else: # Just add a  dot to the list  
 				self.clicks = 1
 				self.temp_X.append(self.x_coord) # by value or by reference?
 				self.temp_Y.append(self.y_coord)
-		A = nx.adjacency_matrix(self.G)
+		#A = nx.adjacency_matrix(self.G)
 		for index in range(self.i):
-                        if self.G.degree(index) in self.colors:
-                                self.c[index] = self.colors[self.G.degree(index)]
-                        else:
-                                self.c[index] = 99
-                print self.c
-                self.ax.scatter(self.vert_x,self.vert_y, s= self.r, c=self.c, cmap = mpl.cm.Blues)
+                        #if self.G.degree(index) in self.colors:
+                        #        self.c[index] = self.colors[self.G.degree(index)]
+                        #else:
+                        #        self.c[index] = 99
+                #print self.c
+			self.ax.scatter(self.vert_x,self.vert_y, s= self.r, c=self.c, cmap = mpl.cm.Blues)
 		for j in range(len(self.edge_X)):
 			self.ax.plot(self.edge_X[j], self.edge_Y[j], self.edge_colors[j])
 		self.canvas.draw()
@@ -96,7 +97,28 @@ class GraphApp:
 				return 1, self.vert_x[i], self.vert_y[i]
 		return 0, x, y
 
-	
+	def clearPlot(self):
+		self.fig.clear()
+		self.vert_x = []
+		self.vert_y = []
+		self.edge_X = []
+		self.edge_Y = []
+		self.r = []
+		self.c = []
+		self.i = 0
+		self.edge_colors = []
+		self.edge_indices = []
+		for index in range(self.i):
+                        #if self.G.degree(index) in self.colors:
+                        #        self.c[index] = self.colors[self.G.degree(index)]
+                        #else:
+                        #        self.c[index] = 99
+                #print self.c
+			self.ax.scatter(self.vert_x,self.vert_y, s= self.r, c=self.c, cmap = mpl.cm.Blues)
+		for j in range(len(self.edge_X)):
+			self.ax.plot(self.edge_X[j], self.edge_Y[j], self.edge_colors[j])
+		self.canvas.draw()
+
 if __name__ == '__main__':
 	root = Tk.Tk()
 	fig = Figure(facecolor = 'white')
@@ -105,13 +127,18 @@ if __name__ == '__main__':
 	ax1.set_ylim(0,1)
 	ax1.axis('off')
 	canvas = FigureCanvasTkAgg(fig, master=root)
-	cp = GraphApp(canvas, ax1)
+	cp = GraphApp(canvas, fig, ax1)
 	canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+	
 	def _quit():
 		root.quit()     # stops mainloop
 		root.destroy()  # this is necessary on Windows to prevent
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+	def _clear():
+		cp.clearPlot()
 
 	button = Tk.Button(master=root, text='Quit', command=_quit)
+	button.pack(side=Tk.BOTTOM)
+	button = Tk.Button(master=root, text='Clear', command=_clear)
 	button.pack(side=Tk.BOTTOM)
 	Tk.mainloop()
