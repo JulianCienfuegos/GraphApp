13d2580abd1968a3ba64d798b5dacbc45bbd3e79
JulianCienfuegos <melvyniandrag@gmail.com>
2015-09-21 00:27:21 -0400

    done.

--- graph_app.py
+++ graph_app.py
@@ -13,6 +13,7 @@ import random
 import sys
 import math
 import time
+from params import *
 
 mpl.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -21,25 +22,6 @@ from matplotlib.figure import Figure
 import Tkinter as Tk
 
 mpl.rcParams['toolbar'] = 'None'
-
-color_map = {'b' : 1,
-			 'g' : 2,
-			 'r' : 3,
-			 'c' : 4,
-			 'm' : 5,
-			 'y' : 6, 
-			 'k' : 7,
-			 'w' : 8}
-
-reverse_map = {1 : 'b',
-			   2 : 'g',
-			   3 : 'r',
-			   4 : 'c',
-			   5 : 'm',
-			   6 : 'y',
-			   7 : 'k',
-			   8 : 'w'}
-edge_color = 'k'
 
 class GraphApp:
 	def __init__(self, canvas, fig, ax1):	
@@ -71,12 +53,14 @@ class GraphApp:
 			self.vert_x.append(event.xdata)
 			self.vert_y.append(event.ydata) # Dot ready for drawing.
 			self.r.append(200)
-			col = random.randint(1,8)
-			self.c.append(reverse_map[col])
+			color = random.randint(1,8)
+			self.c.append(reverse_map[color])
 			self.G.add_node(self.num_verts)
 			e = (self.num_verts, self.num_verts)
 			self.G.add_edge(*e)
 			self.num_verts += 1
+			# DRAW A VERTEX
+			self.draw_vert(event.xdata, event.ydata, color)
 		else: # then the click is close
 			if self.clicks == 1 and self.temp_X != [] and self.temp_X[0] == self.x_coord and self.temp_Y[0] == self.y_coord:
 				self.clicks = 0
@@ -91,17 +75,19 @@ class GraphApp:
 				e = (self.vert_x.index(self.temp_X[0]), self.vert_x.index(self.temp_X[1]))
 				edge_idx_tup = (e)
 				self.G.add_edge(*e)
+				# DRAW AN EDGE
+				self.draw_edge(self.temp_X, self.temp_Y)
 				self.temp_X = []
 				self.temp_Y = []
 			else: # Just add a  dot to the list  
 				self.clicks = 1
 				self.temp_X.append(self.x_coord) 
 				self.temp_Y.append(self.y_coord)
-		self.draw_plot()
+		#self.draw_plot()
 		
 	def distance(self, x, y):
-		"""Check all of the coordinates in the vert_x, vert_y lists. If there is a coordinate near to the input x, y 
-		then return near = 1, and x, y equal the found coordinates."""
+		'''Check all of the coordinates in the vert_x, vert_y lists. If there is a coordinate near to the input x, y 
+		then return near = 1, and x, y equal the found coordinates.'''
 		for i, v in enumerate(self.vert_x):
 			if math.sqrt((x - self.vert_x[i])**2+(y - self.vert_y[i])**2) < self.tol:
 				return 1, self.vert_x[i], self.vert_y[i]
@@ -132,6 +118,7 @@ class GraphApp:
 		self.temp_Y = []
 
 	def color_iteration(self):
+		''' This function updates the colors of the vertices using  a simple rule.'''
 		c_nums = [color_map[c] for c in self.c]
 		c_vec = np.dot(self.adj_mat, c_nums)
 		self.c = [reverse_map[math.floor(i)] for i in c_vec]
@@ -140,11 +127,18 @@ class GraphApp:
 	def draw_plot(self):
 		''' This should be sped up because it takes a very long time when there are ten or so vertices.'''
 		for index in range(self.num_verts):
-			self.ax.scatter(self.vert_x,self.vert_y, s= self.r, c=self.c, cmap='hsv')
+			self.ax.scatter(self.vert_x,self.vert_y, s= self.r, c=self.c)
 		for j in range(len(self.edge_X)):
 			self.ax.plot(self.edge_X[j], self.edge_Y[j], c = edge_color)
 		self.canvas.draw()
 
+	def draw_edge(self, e_x, e_y):
+		self.ax.plot(e_x, e_y, c = edge_color)
+		self.canvas.draw()
+
+	def draw_vert(self, v_x, v_y, v_color):
+		self.ax.scatter(v_x, v_y, s = self.r, c = reverse_map[v_color])
+		self.canvas.draw()
 	# We need to define a little arithmetic for working with color names, because the color numbers aren't working correctly.
 
 if __name__ == '__main__':
@@ -160,9 +154,10 @@ if __name__ == '__main__':
 	canvas.get_tk_widget().pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
 
 	def _update_colors():
-		for i in range(15):
+		num_iters = int(entry.get())
+		for i in range(num_iters):
 			cp.color_iteration()
-			time.sleep(1.0)
+			time.sleep(0.25)
 
 	def _quit():
 		root.quit()     
@@ -174,12 +169,16 @@ if __name__ == '__main__':
 	def _view_adj():
 		print(cp.adj_mat)
 
-	button = Tk.Button(master=root, text='Run', command=_update_colors, height = 10, width = 10)
+	label = Tk.Label(text='Num Iters')
+	label.pack(side=Tk.TOP)
+	entry = Tk.Entry(root, width=10)
+	entry.pack(side=Tk.TOP)
+	button = Tk.Button(master=root, text='Run', command=_update_colors, height = 2, width = 10)
 	button.pack(side=Tk.TOP)
-	button = Tk.Button(master=root, text='Clear', command=_clear, height = 10, width = 10)
+	button = Tk.Button(master=root, text='Adjacency', command=_view_adj, height = 2, width = 10)
 	button.pack(side=Tk.TOP)
-	button = Tk.Button(master=root, text='Quit', command=_quit, height = 10, width = 10)
+	button = Tk.Button(master=root, text='Clear', command=_clear, height = 2, width = 10)
 	button.pack(side=Tk.TOP)
-	button = Tk.Button(master=root, text='Adjacency', command=_view_adj, height = 10, width = 10)
+	button = Tk.Button(master=root, text='Quit', command=_quit, height = 2, width = 10)
 	button.pack(side=Tk.TOP)
 	Tk.mainloop()
